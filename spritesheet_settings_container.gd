fb4d776f23c41b3543965aa5ce876ceebcce7201
Chanz Jryko Langreo <langreo.grim@gmail.com>
2026-03-23 16:32:16 +0800

    animated settings visibility init

--- spritesheet_settings_container.gd
+++ spritesheet_settings_container.gd
@@ -1,14 +1,25 @@
 extends PanelContainer
 
-
 @export var enabled_checkbox: CheckBox
 @export var settings_container: PanelContainer
+
+@export var animated_checkbox: CheckBox
+@export var frame_spiner_container: HBoxContainer
+@export var frames_container: VBoxContainer
 
 
 func _ready() -> void:
   enabled_checkbox.toggled.connect(_toggle_settings_container_visibility)
-  _toggle_settings_container_visibility(!enabled_checkbox.toggle_mode)
+  _toggle_settings_container_visibility(false)
 
+
+  animated_checkbox.toggled.connect(_toggle_animatable)
+  _toggle_animatable(false)
 
 func _toggle_settings_container_visibility(toggled_on: bool) -> void:
   settings_container.visible = toggled_on
+
+
+func _toggle_animatable(toggled_on: bool) -> void:
+  frames_container.visible = toggled_on
+  frame_spiner_container.visible = !toggled_on
